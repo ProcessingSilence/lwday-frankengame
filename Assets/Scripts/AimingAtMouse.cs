9b6fa0ef2d653014e50eec29336d900d5f36521e
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-04 23:22:04 -0400

    11:21 PM

--- Assets/Scripts/AimingAtMouse.cs
+++ Assets/Scripts/AimingAtMouse.cs
@@ -7,11 +7,12 @@ public class AimingAtMouse : MonoBehaviour
 {
     public Vector2 mousePos, myPos;
     public float angle;
+    public SpriteRenderer childSpriteRenderer;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        childSpriteRenderer = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,6 +21,18 @@ public class AimingAtMouse : MonoBehaviour
         myPos = Camera.main.WorldToViewportPoint(transform.position);
         mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         angle = (float) Math.Atan2(myPos.y - mousePos.y, myPos.x - mousePos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle - 180));
+    }
+
+    private void LateUpdate()
+    {
+        if (angle > 90 || angle < -90)
+        {
+            childSpriteRenderer.flipY = false;
+        }
+        else if (angle < 90 && angle > -90)
+        {
+            childSpriteRenderer.flipY = true;
+        }
     }
 }
