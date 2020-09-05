d4c95a2f556ae00c402daa6c20769bf6c7e8951f
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-05 11:48:52 -0400

    11:48 PM

--- Assets/Scripts/Global/Enemy/LimpCorpse.cs
+++ Assets/Scripts/Global/Enemy/LimpCorpse.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LimpCorpse : MonoBehaviour
+public class LimpCorpse : StateMachineBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Color deathColor = new Color(128,128,128);
+    private LimpCorpse LimpCorpse_script;
 
-    // Update is called once per frame
-    void Update()
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
     {
-        
+        // Set Layer to "Corpse"
+        if (animator.gameObject.layer != 11)
+        {
+            animator.gameObject.layer = 11;
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.gravityScale = 1;
+            spriteRenderer = animator.gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sortingOrder = -1;
+            LimpCorpse_script = animator.gameObject.GetComponent<LimpCorpse>();
+        }
+        else if (spriteRenderer.color.r > deathColor.r)
+        {
+            spriteRenderer.color -= new Color(1,1,1);
+        }
+        else
+        {
+            animator.SetBool("isDead", false);
+            animator.SetBool("disableAll", true);
+        }
     }
 }
