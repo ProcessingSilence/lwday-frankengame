3bc1715be2ce5a8be3f203fbf497ad033e0ea600
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-09 02:19:14 -0400

    2:19 AM

---
+++ Assets/physicsObj.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class physicsObj : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
