5fd2435f2f6a5cc0239990f5775e2f7ae6f79d2e
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-08 20:04:06 -0400

    8:04 PM

---
+++ Assets/Scripts/SpawnGoalUponDeaths.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGoalUponDeaths : MonoBehaviour
+{
+    public Transform[] enemiesToKill;
+    private bool confirmGoal;
+    //public string FindObjTag;
+    public GameObject GoalObj;
+    private AudioSource audioSource;
+
+    private ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        particleSystem = GetComponent<ParticleSystem>();
+        GoalObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEmpty(enemiesToKill) && confirmGoal == false)
+        {
+            confirmGoal = true;
+            StartCoroutine(SpawnGoal());
+        }
+    }
+    
+    private bool isEmpty(Transform[] arr)
+    {
+        for(int i = 0; i < arr.Length; i++) 
+        {
+            if (arr[i] != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnGoal()
+    {
+        yield return new WaitForSecondsRealtime(0.1f);
+        transform.position = GoalObj.transform.position;
+        GoalObj.SetActive(true);
+        audioSource.Play();
+        particleSystem.Play();
+    }
+}
